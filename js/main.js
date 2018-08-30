cea6c0ec0caf404063d3cc5c8434e8b42c1c38d2
Sonali Shukla <sonalikatara@gmail.com>
2018-08-30 11:38:44 -0700

    responsive desn: add meta viewport and start on resturant-container

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1Ijoic29uYWxpayIsImEiOiJjamxnNGptMGcwZzdoM3Fud2JwejhxYnBxIn0.bu5d4lUUaRQZiTu1A2MAnQ',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
